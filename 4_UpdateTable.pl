3aeadfe1732583aac7c0b0be86a5a1057457804e
Suganthikumar <suganthikumar01@gmail.com>
2019-04-30 13:33:16 +0530

    commit by sugkumar

--- 4_UpdateTable.pl
+++ 4_UpdateTable.pl
@@ -10,10 +10,13 @@ print "\n Enter email111 ";
 $email = <>;
 chomp($email);
 print "Enter new password";
+print "Enter new234r5tyui password";
 $password = <>;
 chomp($password);
 chomp($password1);
 $sth = $dbh->prepare("update users set password = ? where email = ?");
 $sth->execute($password, $email) or die $DBI::errstr;
 $sth->finish();
+$sth1->finish();
+
 
